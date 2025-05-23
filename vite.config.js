09adf38fcd3973805d6f31c639a34e067bb94c43
mahajang214 <nayanmahajan91@gmail.com>
2025-05-23 12:36:00 +0530

    going to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig({base: '/portfolio/',
   plugins: [react(),tailwindcss()],
 })
